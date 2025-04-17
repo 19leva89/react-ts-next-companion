9938e2219ed576db711736d3e9c4102651b258ea
Sobolev Dmitry <19leva89@gmail.com>
2025-04-17 22:59:54 +0300

    project init

---
+++ src/components/ui/index.ts
@@ -0,0 +1,37 @@
+export { Avatar, AvatarFallback, AvatarImage } from './avatar'
+
+export { Button, buttonVariants } from './button'
+export { Card, CardHeader, CardFooter, CardTitle, CardDescription, CardContent } from './card'
+export {
+	Dialog,
+	DialogContent,
+	DialogDescription,
+	DialogFooter,
+	DialogHeader,
+	DialogTitle,
+	DialogTrigger,
+} from './dialog'
+export {
+	DropdownMenu,
+	DropdownMenuCheckboxItem,
+	DropdownMenuContent,
+	DropdownMenuItem,
+	DropdownMenuTrigger,
+} from './dropdown-menu'
+export { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from './form'
+export { Input } from './input'
+export { Label } from './label'
+export { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from './select'
+export { Separator } from './separator'
+export {
+	Sheet,
+	SheetClose,
+	SheetContent,
+	SheetDescription,
+	SheetFooter,
+	SheetHeader,
+	SheetTitle,
+	SheetTrigger,
+} from './sheet'
+export { Textarea } from './textarea'
+export { Toaster } from './sonner'
