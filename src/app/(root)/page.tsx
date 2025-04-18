1f1607a2760d6c5def7162f3f7c73fd03cd65898
Dmitry Soboliev <19leva89@gmail.com>
2025-04-18 10:01:29 +0300

    Environment Setup + Folder Setup + add Clerk Authentication

---
+++ src/app/(root)/page.tsx
@@ -0,0 +1,50 @@
+// import { prisma } from '@/lib/prisma'
+// import Companions from '@/components/companions'
+// import Categories from '@/components/categories'
+// import SearchInput from '@/components/search-input'
+
+import { UserButton } from '@clerk/nextjs'
+
+interface Props {
+	searchParams: {
+		categoryId: string
+		name: string
+	}
+}
+
+const RootPage = async ({ searchParams }: Props) => {
+	// const data = await prisma.companion.findMany({
+	// 	where: {
+	// 		categoryId: searchParams.categoryId,
+	// 		name: {
+	// 			search: searchParams.name,
+	// 		},
+	// 	},
+	// 	orderBy: {
+	// 		createdAt: 'desc',
+	// 	},
+	// 	include: {
+	// 		_count: {
+	// 			select: {
+	// 				messages: true,
+	// 			},
+	// 		},
+	// 	},
+	// })
+
+	// const categories = await prisma.category.findMany()
+
+	return (
+		<div className="h-full p-4 space-y-2">
+			{/* <SearchInput /> */}
+
+			{/* <Categories data={categories} /> */}
+
+			{/* <Companions data={data} /> */}
+
+			<UserButton />
+		</div>
+	)
+}
+
+export default RootPage
